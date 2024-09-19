e30cca096fc5f4c570d95793b1649e56b73303f6
lwambon <lwambonyaga@gmail.com>
2024-09-19 21:32:55 +0300

    rock paper scissor game

---
+++ script.js
@@ -0,0 +1,8 @@
+const computersChoice =document.getElementById("computers-choice")
+const playersChoice =document.getElementById("players-choice")
+const result =document.getElementById("result")
+const choicesWin = document.getElementById("choices-win")
+
+function computerChoice() {
+    const randomNumber = math.floor(Math.random(0,2)*10)
+}
