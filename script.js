7e7d86faeb504dbe4e7b639321e0c2c945b81315
lwambon <lwambonyaga@gmail.com>
2024-09-20 15:35:53 +0300

    jss code for rock paper scissor game

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
-const computersChoice =document.getElementById("computers-choice")
-const playersChoice =document.getElementById("players-choice")
-const result =document.getElementById("result")
-const choicesWin = document.getElementById("choices-win")
+const playerOption = ["rock", "paper","scissor","paper", 
+    "rock","scissor","paper", "rock","scissor","rock", "paper","scissor","rock", "scissor","paper",
+    "rock", "paper","scissor"]
 
-function computerChoice() {
-    const randomNumber = math.floor(Math.random(0,2)*10)
+const playerOptionIcon ={
+    rock: "&#9994;",
+    paper: "&#9995;",
+    scissor: "&#9996;"
+}
+
+const computerDispayChoice = document.getElementsByClassName("computer-dispay-choice")[0];
+const scoreResult = document.getElementsByClassName("result")[0];
+const playerDispayChoice = document.getElementsByClassName("player-dispay-choice")[0];
+const computerCount = document.getElementsByClassName("computer-count")[0];
+const playerCount = document.getElementsByClassName("player-count")[0];
+
+let defaultChoice = "scissor";
+
+function initializeDefaultChoice() {
+    playerDispayChoice.innerHTML = playerOptionIcon[defaultChoice];
+    computerDispayChoice.innerHTML = playerOptionIcon[defaultChoice];
+    scoreResult.textContent = "currently draw";
+}
+
+console.log(initializeDefaultChoice());
+
+
+function playgame (playerChoice){
+    const computerChoice = playerOption[Math.floor(Math.random()*3)];
+    let result;
+
+    if(computerChoice === playerChoice){
+        result = "draw"
+    }else if(computerChoice ==="rock" && playerChoice ==="scissor"){
+        result ="computer wins"
+    }else if(computerChoice ==="scissor" && playerChoice ==="rock"){
+        result ="player wins"
+    }else if(computerChoice ==="paper" && playerChoice ==="scissor"){
+        result ="player wins"
+    }else if(computerChoice ==="rock" && playerChoice ==="paper"){
+        result ="player wins"
+    }else if(computerChoice ==="scissor" && playerChoice ==="paper"){
+        result ="computer wins"
+    }else if(computerChoice ==="paper" && playerChoice ==="rock"){
+        result ="computer wins"
+    }  
+    
+    playerDispayChoice.innerHTML = playerOptionIcon[playerChoice];
+    computerDispayChoice.innerHTML = playerOptionIcon[computerChoice];
+    scoreResult.textContent = result;
+
+    if (result ==="computer wins"){
+        computerCount.textContent = parseInt(computerCount.textContent)+1;
+    }else if (result ==="player wins"){
+        playerCount.textContent = parseInt(playerCount.textContent)+1;
+    }
 }
